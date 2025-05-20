71cfb89a0eee9de50d49f9fa591ecc84f610c3c2
Morel <seriaofever@gmail.com>
2025-05-20 19:54:49 +0800

    增加重构params和headers的方法

--- aa.py
+++ aa.py
@@ -1,6 +1,7 @@
 import json
 
 import pydash
+
 # todo  用自己封装的方法
 from loguru import logger
 import requests
@@ -15,7 +16,7 @@ class PageSama:
     headers: dict = field(default_factory=dict)  # 默认为空字典
     cookies: dict = field(default_factory=dict)  # 默认为空字典
     proxy: dict = field(default_factory=dict)  # 默认为空字典
-    max_page: int = 1  # 默认为 1
+    max_page: int = 1  # default 1
 
     def __post_init__(self):
         self.page_axios = list((self.post_data or self.get_params).keys())[0]
@@ -28,11 +29,39 @@ class PageSama:
     def fetch_one_page(self, page_num):
         # todo
         if self.is_get_method:
-            res = requests.get(self.url, params=self.get_params, headers=self.headers, cookies=self.cookies,
-                               proxies=self.proxy)
+            # 更改页数
+            self.get_params.update({self.page_axios: page_num})
+            # remake params
+            if self.upd_params():
+                self.get_params.update(self.upd_params())
+                
+            # remake headers
+            if self.upd_hd():
+                self.headers.update(self.upd_hd())
+            res = requests.get(
+                self.url,
+                params=self.get_params,
+                headers=self.headers,
+                cookies=self.cookies,
+                proxies=self.proxy,
+            )
         else:
+            self.post_data.update({self.page_axios: page_num})
             req_data = json.dumps(self.post_data)
-            res = requests.post(self.url, data=req_data, headers=self.headers, cookies=self.cookies, proxies=self.proxy)
+            # remake params
+            if self.upd_params():
+                self.get_params.update(self.upd_params())
+
+            # remake headers
+            if self.upd_hd():
+                self.headers.update(self.upd_hd())
+            res = requests.post(
+                self.url,
+                data=req_data,
+                headers=self.headers,
+                cookies=self.cookies,
+                proxies=self.proxy,
+            )
 
         return res.json()
 
@@ -40,8 +69,12 @@ class PageSama:
         parsed_items = pydash.get(res_json, item_path) or []
         return parsed_items
 
+    def upd_hd(self): ...
 
-if __name__ == '__main__':
+    def upd_params(self): ...
+
+
+if __name__ == "__main__":
     # todo  1.无差别翻页
     # 2. 找一个测试的网站，manga porn
     PageSama()
