3da0e378d99bbafafae1c7de0f222a67003c43f4
Morel <seriaofever@gmail.com>
2025-05-23 17:50:30 +0800

    增加递归结构的todo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # stealdr
-A simple and handy spider framework specially for turning pages of xhr requests
+
+Tired of writing for loop on turning pages of xhr spiders?
+stealdr offers a better option .The features are below
+- set max page for your for loop and collect response of each requests
+- multithread requests
+- elegant parse path
+- no need to set method on get or post.Just pass a dict to the get_params or post_data
+- this framework takes the 1st key of the dict as page turner .plz be aware
+
+later to do 
+- log to mongo col
+- update new header n params on each requests
--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ class PageSama:
     is_remote_log: bool = False
     item_path_key: str = ''
     detail_path_list: list = field(default_factory=list)
+    break_path: str = ''
 
     def __post_init__(self):
         self.page_axios = list((self.post_data or self.get_params).keys())[0]
@@ -38,24 +39,34 @@ class PageSama:
             self.detail_path_list = self.item_path_dict.get(self.item_path_key)
 
     def mix_or_pick(self):
-        page_index_list = [self.start_offset + i * self.offset for i in range(self.max_page)]
-        if self.multi_thread:
-            with ThreadPoolExecutor() as executor:
-                tmp = executor.map(lambda pdex: self.peel(self.fetch_one_page(pdex)), page_index_list)
+        if self.break_path:
+            for one_res in self.fetch_one_page():
+                if break_point := pydash.get(one_res, self.break_path):
+                    # continue iteration
+                    # todo 
+                    pass
+                else:
+                    break
+        else:
+            page_index_list = [self.start_offset + i * self.offset for i in range(self.max_page)]
+            if self.multi_thread:
+                with ThreadPoolExecutor() as executor:
+                    tmp = executor.map(lambda pdex: self.peel(self.fetch_one_page(pdex)), page_index_list)
 
-        else:
-            tmp = [self.peel(self.fetch_one_page(pdex)) for pdex in page_index_list]
-        if self.item_path_key:
-            page_res = list(chain.from_iterable(tmp))
-            return [
-                [pydash.get(one, path_) for one in page_res]
-                for path_ in self.detail_path_list
-            ]
-        else:
-            return tmp
+            else:
+                tmp = [self.peel(self.fetch_one_page(pdex)) for pdex in page_index_list]
+            if self.item_path_key:
+                page_res = list(chain.from_iterable(tmp))
+                return [
+                    [pydash.get(one, path_) for one in page_res]
+                    for path_ in self.detail_path_list
+                ]
+            else:
+                return tmp
 
     @logger.catch
     def fetch_one_page(self, page_num):
+        # todo ? maybe rewrite
         if self.is_get_method:
             current_params = self.get_params.copy()
             # 更改页数
@@ -110,7 +121,7 @@ class PageSama:
         ...
 
 
-if __name__ == "__main__":
+def all_normal_pages():
     url = "https://spa1.scrape.center/api/movie/"
     params = {
         "offset": "10",
@@ -121,3 +132,27 @@ if __name__ == "__main__":
     instance_a = PageSama(url=url, get_params=params, offset=20, max_page=3, item_path_dict=parse_path_dict)
     # instance_a = PageSama(url=url, get_params=params, offset=20, max_page=3, )
     print(list(instance_a.mix_or_pick()))
+
+
+def turn_with_iteration():
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    }
+    item_path_dict = ''  # todo
+    params = {
+        # 'offset': 'heat_2913317_18_20', # todo
+        'offset': '',
+        'topic_id': '1308237',
+        'sort_by': '0',
+        'page_size': '20',
+        'source': 'Web',
+        'features': 'itemOpusStyle,listOnlyfans,opusBigCover,onlyfansVote,decorationCard',
+    }
+
+    url = 'https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/topic'
+    path_str = 'data.topic_card_list.offset'
+    instance_a = PageSama(url=url, get_params=params, break_path=path_str)
+
+
+if __name__ == "__main__":
+    turn_with_iteration()
